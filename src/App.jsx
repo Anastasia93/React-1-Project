b35bb5cb90afe508e4c3d17e9e4369c6abff96e2
Anastasiia Yahniuk <anastasiia.yahniuk@gmail.com>
2025-08-14 14:15:00 +0200

    Adds basic props usage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
+
 const reactDescriptions = ['Fundamental', 'Crutual', 'Core'];
 
 function getRandomInt(max) {
-  return Math.floor(Math.random()*(max+1));
+  return Math.floor(Math.random() * (max + 1));
 };
 
 function Header() {
-const description = reactDescriptions[getRandomInt(2)];
+  const description = reactDescriptions[getRandomInt(2)];
 
   return (<header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <img src={reactImg} alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
       {description} React concepts you will need for almost any app you are
@@ -17,12 +20,40 @@ const description = reactDescriptions[getRandomInt(2)];
   </header>);
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}></img>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              {...CORE_CONCEPTS[1]}
+            />
+            <CoreConcept
+              {...CORE_CONCEPTS[2]}
+            />
+            <CoreConcept
+              {...CORE_CONCEPTS[3]}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
