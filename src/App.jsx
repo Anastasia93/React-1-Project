3b3d2534607c1016b5ba6ff762f08692cadda8a7
Anastasiia Yahniuk <anastasiia.yahniuk@gmail.com>
2025-08-20 12:24:31 +0200

    Add basic tab selection handle.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  let tabContent = "Some content.";
+
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+    tabContent = selectedButton;
+  }
+
+
   return (
     <div>
       <Header />
@@ -31,11 +39,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
