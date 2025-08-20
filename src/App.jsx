a21a45f60023fb3c9230652f85f1b11ab62d5b67
Anastasiia Yahniuk <anastasiia.yahniuk@gmail.com>
2025-08-20 13:30:38 +0200

    Updates the tabs information.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
+
 import { CORE_CONCEPTS } from "./data.js";
+import { EXAMPLES } from "./data.js";
 
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  let tabContent = "Some content.";
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
-  function handleSelect(selectedButton) {
-    console.log(selectedButton);
-    tabContent = selectedButton;
+  function handleSelect(selectedButtonValue) {
+    setSelectedTopic(selectedButtonValue);
+    console.log(selectedButtonValue);
   }
 
 
@@ -44,7 +47,13 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {tabContent}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
