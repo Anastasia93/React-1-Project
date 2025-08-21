ce215ca6e0daed302cb0eecb5ba9b35404ac9c63
Anastasiia Yahniuk <anastasiia.yahniuk@gmail.com>
2025-08-21 12:01:16 +0200

    Adds active class flow to the tabs.

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   let tabContent = <p>Please, select a topic.</p>;
 
   if (selectedTopic) {
-    tabContent = (<div id="tab-content">
+    tabContent = (<div>
       <h3>{EXAMPLES[selectedTopic].title}</h3>
       <p>{EXAMPLES[selectedTopic].description}</p>
       <pre>
@@ -53,12 +53,14 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {tabContent}
+          <div id="tab-content">
+            {tabContent}
+          </div>
         </section>
       </main>
     </div>
